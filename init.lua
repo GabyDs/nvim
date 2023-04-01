13be7d36c99811c9d918a410716fa375279de787
GabyDs <gabydsch2001@gmail.com>
2023-04-01 18:51:15 -0300

    Create folders to plugins an config files

---
+++ init.lua
@@ -0,0 +1,4 @@
+require('keymaps')		----- keymaps
+require('plugins/ayu')		----- color theme
+require('plugins/packer')	----- plugin manager
+require('settings')		----- settings
---
+++ lua/keymaps.lua
@@ -0,0 +1,5 @@
+local maps = { i = {}, n = {}, v = {}, t = {} }
+
+maps.n["<leader>e"] = { "<cmd>Neotree toggle<cr>", desc = "Toggle Explorer" }
+maps.n["<leader>o"] = { "<cmd>Neotree focus<cr>", desc = "Focus Explorer" }
+
